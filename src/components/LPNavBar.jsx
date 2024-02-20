f29f1b565bab991eb2ed6023186caa8f74572795
Jason-Mendes <jaxmendes2@gmail.com>
2024-02-20 02:19:24 +0200

    made the user profile link responsive and installed react icons using npm created menus for OP nav bar

--- src/components/LPNavBar.jsx
+++ src/components/LPNavBar.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom';
 function LPNavBar() {
   // console.log('Rendering LPNavBar');
     return (
-        <nav className="bg-slate-100 text-orange-500 p-4">
+        <nav className="bg-slate-100 text-black p-4">
         <ul className="flex space-x-4">
           <li>
-            <Link to="/LandingPage" className="hover:text-black">LandingPage</Link>
+            <Link to="/LandingPage" className="hover:text-white">LandingPage</Link>
           </li>
           <li>
-            <Link to="/home" className="hover:text-black">Home</Link>
+            <Link to="/home" className="hover:text-white">Home</Link>
           </li>
           <li>
-            <Link to="/products" className="hover:text-black">Products</Link>
+            <Link to="/products" className="hover:text-white">Products</Link>
           </li>
           <li>
-            <Link to="/cart" className="hover:text-black">Cart</Link>
+            <Link to="/cart" className="hover:text-white">Cart</Link>
           </li>
           <li>
-            <Link to="/user-profile" className="hover:text-black">User Profile</Link>
+            <Link to="/user-profile" className="hover:text-white">User Profile</Link>
           </li>
         </ul>
       </nav>
--- src/components/OPNavBar.jsx
+++ src/components/OPNavBar.jsx
@@ -1,30 +1,64 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-function OPNavBar() {
+import { CgMenuRound, CgMenuOreos, CgMenuCake, CgClose } from "react-icons/cg";
+import { Link } from 'react-router-dom';function OPNavBar() {
   // console.log('Rendering OPNavBar');
-    return (
-        <nav className="bg-slate-100 text-black p-4">
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/LandingPage" className="hover:text-white">LandingPage</Link>
+  return (
+      <nav className="bg-slate-100 text-orange-500 p-5">
+      <div className="flex justify-between items-center max-w-[1350px] mx-auto px-4">
+
+        <div className="flex items-center">
+          <h1 className='text-3xl font-bold text-[#df7000] whitespace-nowrap hidden'>Etomart</h1>
+        </div>
+
+        <ul className="flex hidden">
+          <li className='p-4 whitespace-nowrap'>
+            <Link to="/LandingPage" className="hover:text-black">LandingPage</Link>
           </li>
-          <li>
-            <Link to="/home" className="hover:text-white">Home</Link>
+          <li className='p-4 whitespace-nowrap'>
+            <Link to="/home" className="hover:text-black">Home</Link>
           </li>
-          <li>
-            <Link to="/products" className="hover:text-white">Products</Link>
+          <li className='p-4 whitespace-nowrap'>
+            <Link to="/products" className="hover:text-black">Products</Link>
           </li>
-          <li>
-            <Link to="/cart" className="hover:text-white">Cart</Link>
+          <li className='p-4 whitespace-nowrap'>
+            <Link to="/cart" className="hover:text-black">Cart</Link>
           </li>
-          <li>
-            <Link to="/user-profile" className="hover:text-white">User Profile</Link>
+          <li className='p-4 whitespace-nowrap'>
+            <Link to="/user-profile" className="hover:text-black">User Profile</Link>
           </li>
         </ul>
-      </nav>
+
+        <div>
+          < CgMenuRound size={35}/>
+        </div>
+        <div className='text-[#ffffff] fixed left-0 top-0 w-[60%] h-full border-r uppercase bg-orange-500'>
+        
+        <h1 className='text-3xl font-bold text-[#ffffff] whitespace-nowrap m-4'>Etomart</h1>
+
+          <ul className='uppercase p-2'>
+          <li className='p-4 whitespace-nowrap border-b'>
+            <Link to="/LandingPage" className="hover:text-black">LandingPage</Link>
+          </li>
+          <li className='p-4 whitespace-nowrap border-b'>
+            <Link to="/home" className="hover:text-black">Home</Link>
+          </li>
+          <li className='p-4 whitespace-nowrap border-b'>
+            <Link to="/products" className="hover:text-black">Products</Link>
+          </li>
+          <li className='p-4 whitespace-nowrap border-b'>
+            <Link to="/cart" className="hover:text-black">Cart</Link>
+          </li>
+          <li className='p-4 whitespace-nowrap'>
+            <Link to="/user-profile" className="hover:text-black">User Profile</Link>
+          </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
     );
 }
 
 export default OPNavBar;
+
 //Other Pages Nav Bar
