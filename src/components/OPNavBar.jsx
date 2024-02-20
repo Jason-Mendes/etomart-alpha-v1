e0da0a17eae788618518cdedaed6283ef5aad7ce
odellmendes <jaxmendes2@gmail.com>
2024-02-20 16:25:32 +0200

    Other Pages Nav Bar implemented

--- src/components/OPNavBar.jsx
+++ src/components/OPNavBar.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CgMenuRound, CgMenuOreos, CgMenuCake, CgClose } from "react-icons/cg";
-import { Link } from 'react-router-dom';function OPNavBar() {
-  // console.log('Rendering OPNavBar');
+import { Link } from 'react-router-dom';
+
+
+function OPNavBar() {
+  // console.log('Rendering OPNaxvBar');
+  
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+
   return (
-      <nav className="bg-slate-100 text-orange-500 p-5">
+      <nav className="bg-slate-100 text-orange-500 p-2">
       <div className="flex justify-between items-center max-w-[1350px] mx-auto px-4">
 
         <div className="flex items-center">
-          <h1 className='text-3xl font-bold text-[#df7000] whitespace-nowrap hidden'>Etomart</h1>
+          <h1 className='text-3xl font-bold text-[#df7000] whitespace-nowrap'>Etomart</h1>
         </div>
 
-        <ul className="flex hidden">
+        <ul className="hidden md:flex">
           <li className='p-4 whitespace-nowrap'>
             <Link to="/LandingPage" className="hover:text-black">LandingPage</Link>
           </li>
@@ -28,27 +39,26 @@ import { Link } from 'react-router-dom';function OPNavBar() {
           </li>
         </ul>
 
-        <div>
-          < CgMenuRound size={35}/>
+        <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <CgClose size={30}/> : <CgMenuRound size={30}/>}
         </div>
-        <div className='text-[#ffffff] fixed left-0 top-0 w-[60%] h-full border-r uppercase bg-orange-500'>
-        
-        <h1 className='text-3xl font-bold text-[#ffffff] whitespace-nowrap m-4'>Etomart</h1>
+          <div className={nav ? 'text-[#ffffff] fixed left-0 top-0 w-[60%] h-full border-r uppercase bg-orange-500 ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500'}>
+        <h1 className='text-3xl font-bold text-[#ffffff] whitespace-nowrap mx-4 mt-4 mb-2'>Etomart</h1>
 
           <ul className='uppercase p-2'>
-          <li className='p-4 whitespace-nowrap border-b'>
-            <Link to="/LandingPage" className="hover:text-black">LandingPage</Link>
+          <li className='p-2 whitespace-nowrap border-b'>
+            <Link to="/LandingPage" className="hover:text-black">Landing Page</Link>
           </li>
-          <li className='p-4 whitespace-nowrap border-b'>
+          <li className='p-2 whitespace-nowrap border-b'>
             <Link to="/home" className="hover:text-black">Home</Link>
           </li>
-          <li className='p-4 whitespace-nowrap border-b'>
+          <li className='p-2 whitespace-nowrap border-b'>
             <Link to="/products" className="hover:text-black">Products</Link>
           </li>
-          <li className='p-4 whitespace-nowrap border-b'>
+          <li className='p-2 whitespace-nowrap border-b'>
             <Link to="/cart" className="hover:text-black">Cart</Link>
           </li>
-          <li className='p-4 whitespace-nowrap'>
+          <li className='p-2 whitespace-nowrap'>
             <Link to="/user-profile" className="hover:text-black">User Profile</Link>
           </li>
           </ul>
