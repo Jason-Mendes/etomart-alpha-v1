c2887326379e1918da37fa6972a4692cabc2deb8
odellmendes <jaxmendes2@gmail.com>
2024-05-05 14:41:49 +0200

    Added new Images to authentication screens, created working modals for authentication screens fixed Nav Bar and Modals pop up, created new Modals for extra functions

---
+++ src/components/AuthenticatedSignupModal.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const AuthenticatedSignupModal = ({ showModal, closeModal }) => {
+  return (
+    <>
+      {/* Modal */}
+      <div
+        className={`fixed inset-0 flex items-center justify-center z-50 ${
+          showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+      >
+        <div
+          className={`absolute inset-0 bg-black bg-opacity-50 transition-opacity ${
+            showModal ? 'ease-out duration-700' : 'ease-in duration-700'
+          }`}
+          onClick={closeModal}
+        />
+        <div
+          id="Orange_container"
+          className={`bg-[#ee9613] rounded-lg m-6 p-6 z-50 fixed bottom-6 top-6 left-1/2 transform -translate-x-1/2 transition-all flex flex-col ${
+            showModal ? 'opacity-100 scale-100' : 'opacity-0 scale-95'
+          }`}
+          style={{ width: '90%', maxHeight: '90vh', overflow: 'auto' }}
+        >
+          {/* Your content for the Authenticated modal goes here */}
+          <div className="flex justify-end">
+            <button
+              className="text-[#000000] hover:text-white"
+              onClick={closeModal}
+            >
+              <svg
+                className="mb-4 h-8 w-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+
+          <h2 className="text-center text-2xl font-bold mb-4">
+            Authenticated Sign up
+          </h2>
+          {/* Add your content for the authenticated user here */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuthenticatedSignupModal;
--- src/components/LPNavBar.jsx
+++ src/components/LPNavBar.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import LoginSignupModal from './LoginSignupModal';
+import LoginModal from './LoginModal';
+import SignupModal from './SignupModal';
+import ForgotPasswordModal from './ForgotPasswordModal';
+import AuthenticatedLoginModal from './AuthenticatedLoginModal';
+import AuthenticatedSignupModal from './AuthenticatedSignupModal';
 
 const LPNavBar = () => {
   const [isNavbarSticky, setIsNavbarSticky] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+  const [showForgotPasswordModal, setShowForgotPasswordModal] = useState(false);
+  const [showAuthenticatedLoginModal, setShowAuthenticatedLoginModal] = useState(false); 
+  const [showAuthenticatedSignupModal, setShowAuthenticatedSignupModal] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,6 +27,33 @@ const LPNavBar = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+  const handleLoginClick = () => {
+    setShowLoginModal(true);
+  };
+
+  const handleSignupClick = () => {
+    setShowSignupModal(true);
+  };
+
+  const handleForgotPasswordClick = () => {
+    setShowForgotPasswordModal(true);
+  };
+
+  const handleAuthenticatedLoginClick = () => {
+    setShowAuthenticatedLoginModal(true);
+  };
+
+  const handleAuthenticatedSignupClick = () => {
+    setShowAuthenticatedSignupModal(true);
+  };
+
+  const closeModals = () => {
+    setShowLoginModal(false);
+    setShowSignupModal(false);
+    setShowForgotPasswordModal(false);
+    setShowAuthenticatedLoginModal(false);
+    setShowAuthenticatedSignupModal(false);
+  };
 
   return (
     <div>
@@ -32,16 +68,12 @@ const LPNavBar = () => {
           .flex-container {
             display: flex;
             align-items: center;
-            gap: 6px; /* Adjust the gap as needed */
+            gap: 6px;
           }
         `}
       </style>
 
-      <nav
-  id="lpnavbar"
-  className={`bg-[#f9f9f9] px-4 ${isNavbarSticky ? 'sticky' : ''}`}
->
-
+      <nav id="lpnavbar" className={`bg-[#f9f9f9] px-4 ${isNavbarSticky ? 'sticky' : ''}`}>
         <div className="flex items-center justify-between mx-auto max-w-7xl">
           <div className="flex items-center mt-6 mb-4">
             <h1 className="-mt-2 text-3xl font-shrikhand text-[#ee9613] whitespace-nowrap">
@@ -51,20 +83,76 @@ const LPNavBar = () => {
 
           {/* Buttons container */}
           <div className="flex-container">
-            {/* Render the modal component */}
-            <LoginSignupModal>
-              <button className=" hover:bg-black hover:text-white font-Agbalumo h-[35px] px-4  bg-[#ee9613] text-Black rounded">
-                Login
-              </button>
-            </LoginSignupModal>
-            <LoginSignupModal>
-              <button className=" hover:bg-black hover:text-white  font-Agbalumo h-[35px] px-4  bg-[#ee9613] text-Black rounded">
-                Sign Up
-              </button>
-            </LoginSignupModal>
+            <button onClick={handleLoginClick} className="hover:bg-black hover:text-white font-Agbalumo h-[35px] px-4 bg-[#ee9613] text-Black rounded">
+              Login
+            </button>
+            <button onClick={handleSignupClick} className="hover:bg-black hover:text-white font-Agbalumo h-[35px] px-4 bg-[#ee9613] text-Black rounded">
+              Sign Up
+            </button>
+            <button onClick={handleForgotPasswordClick} className=" hidden hover:bg-black hover:text-white font-Agbalumo h-[35px] px-4 bg-[#ee9613] text-Black rounded">
+            Forgot Password
+            </button>
+            <button onClick={handleAuthenticatedLoginClick} className="hidden hover:bg-black hover:text-white font-Agbalumo h-[35px] px-4 bg-[#ee9613] text-Black rounded">
+            Authenticated Log in
+            </button>
+            <button onClick={handleAuthenticatedSignupClick} className="hidden hover:bg-black hover:text-white font-Agbalumo h-[35px] px-4 bg-[#ee9613] text-Black rounded">
+            Authenticated Sign in
+            </button>
           </div>
         </div>
       </nav>
+
+      {/* Render modals */}
+      <LoginModal
+        showModal={showLoginModal}
+        closeModal={closeModals}
+        openSignupModal={() => {
+          closeModals();
+          setShowSignupModal(true);
+        }}
+        openForgotPasswordModal={() => {
+          closeModals();
+          setShowForgotPasswordModal(true);
+        }}
+        openAuthenticatedLoginModal={() => {
+          closeModals();
+          setShowAuthenticatedLoginModal(true);
+        }}
+        openAuthenticatedSignupModal={() => {
+          closeModals();
+          setShowAuthenticatedSignupModal(true);
+        }}
+      />
+      <SignupModal showModal={showSignupModal} closeModal={closeModals}
+      openLoginModal={() => {
+          closeModals();
+          setShowLoginModal(true);
+        }}
+        openForgotPasswordModal={() => {
+          closeModals();
+          setShowForgotPasswordModal(true);
+        }}
+        openAuthenticatedLoginModal={() => {
+          closeModals();
+          setShowAuthenticatedLoginModal(true);
+        }}
+        openAuthenticatedSignupModal={() => {
+          closeModals();
+          setShowAuthenticatedSignupModal(true);
+        }}
+      />
+      <ForgotPasswordModal
+        showModal={showForgotPasswordModal}
+        closeModal={closeModals}
+      />
+      <AuthenticatedLoginModal
+        showModal={showAuthenticatedLoginModal}
+        closeModal={closeModals}
+      />
+      <AuthenticatedSignupModal
+        showModal={showAuthenticatedSignupModal}
+        closeModal={closeModals}
+      />
     </div>
   );
 };
