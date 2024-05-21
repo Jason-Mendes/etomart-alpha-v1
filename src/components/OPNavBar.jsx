69c429c900a4aa713e044ea14e71e5412eb2f191
odellmendes <jaxmendes2@gmail.com>
2024-05-21 13:56:21 +0200

    imprved the menu button in OP NavBar

--- src/components/OPNavBar.jsx
+++ src/components/OPNavBar.jsx
@@ -74,29 +74,59 @@ function OPNavBar() {
             nav ? 'block' : 'hidden'
           } transition-all duration-500 ease-in-out`}
         >
-          <ul className="uppercase p-2 space-y-2">
-            <li className="p-2 whitespace-nowrap border-b">
-              <Link to="/LandingPage" className="hover:text-black">
-                Landing Page
-              </Link>
-            </li>
-            <li className="p-2 whitespace-nowrap border-b">
-              <Link to="/home" className="hover:text-black">
-                Home
-              </Link>
-            </li>
-            <li className="p-2 whitespace-nowrap border-b">
-              <Link to="/products" className="hover:text-black">
-                Products
-              </Link>
-            </li>
-            <li className="p-2 whitespace-nowrap">
-              <Link to="/user-profile" className="hover:text-black">
-                User Profile
-              </Link>
-            </li>
-          </ul>
-        </div>
+         </div>
+        {nav && (
+          <div
+            className="absolute top-24 right-0 z-20 w-56 bg-[#fdfdfd] rounded-lg shadow-lg transition-opacity duration-200"
+            role="dialog"
+            style={{ opacity: nav ? 1 : 0 }}
+          >
+            <div className="relative">
+              <div className="absolute -top-2.5 right-3 z-20">
+                <svg viewBox="0 0 32 16" className="w- h-4 text-white">
+                  <path className="fill-white "  d="M 16,0 L32,16 H0 Z"></path>
+                  <path 
+                  fill= '#fdfdfd' d="M 16,1 L31,16 H1 Z"></path>
+                </svg>
+              </div>
+              <div className="p-4">
+                <div className="flex items-center justify-center mb-2">
+                <HomeIcon />
+              </div>
+                <div className="flex items-center justify-center mb-2">
+                <CartIcon />
+              </div>
+              <div className="mb-2">
+                  <button className="w-full  py-2 text-center text-orange-500 hover:bg-[#ffaf5e4b] rounded-md">
+                    Login or register
+                  </button>
+                </div>
+                <hr className="border-gray-200" />
+                <div className="mt-2 w-full  py-2 text-center">
+                 
+                    <select
+                      id="language-selector"
+                      className="w-full mt-1 block py-2 px-3 border bg-[#ffaf5e4b] bg-white rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500  sm:text-sm"
+                    >
+                      <option value="en">English</option>
+                      <option value="de">Deutsch</option>
+                      <option value="fr">Français</option>
+                      <option value="es">Español</option>
+                      <option value="ru">Русский</option>
+                      <option value="zh">中文</option>
+                      {/* Add more options as needed */}
+                    </select>
+                 
+                </div>
+                <div className="mt-2">
+                  <button className="w-full px-4 py-2 text-left text-orange-500 hover:bg-[#ffaf5e4b]  rounded-md">
+                    Support
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
